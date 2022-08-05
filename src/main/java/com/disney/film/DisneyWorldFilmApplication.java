01a63fbb2bf9e1e2bb023b660e8799a72501916c
Linda <lindacristal.parrasanhueza@gmail.com>
2022-08-05 20:00:30 -0300

    Initial commit

---
+++ src/main/java/com/disney/film/DisneyWorldFilmApplication.java
@@ -0,0 +1,13 @@
+package com.disney.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyWorldFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyWorldFilmApplication.class, args);
+	}
+
+}
