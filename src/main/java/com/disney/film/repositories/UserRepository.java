a7e9edf5e8d29ecc25791df4a3cd21c004edba90
Linda <lindacristal.parrasanhueza@gmail.com>
2022-08-18 18:45:25 -0300

    Update and delete methods are added

--- src/main/java/com/disney/film/repositories/UserRepository.java
+++ src/main/java/com/disney/film/repositories/UserRepository.java
@@ -4,7 +4,10 @@ import com.disney.film.models.UserModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserModel, Long> {
+    public abstract ArrayList<UserModel> findByPriority(Integer priority);
 
 }
--- src/main/java/com/disney/film/services/UserService.java
+++ src/main/java/com/disney/film/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,5 +19,22 @@ public class UserService {
 
     public UserModel saveUser(UserModel user) {
         return userRepository.save(user);
+    }
+
+    public Optional<UserModel> getById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public ArrayList<UserModel> getByPriority(Integer priority){
+        return userRepository.findByPriority(priority);
+    }
+
+    public Boolean deleteUser(Long id){
+        try {
+            userRepository.deleteById(id);
+            return true;
+        }catch (Exception err){
+            return false;
+        }
     }
 }
