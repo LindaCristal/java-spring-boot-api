a045a9692e1f499aef78febe9a073c0adb519af3
Linda <lindacristal.parrasanhueza@gmail.com>
2022-08-21 22:25:43 -0300

    - added controller for character

--- src/main/java/com/disney/film/controllers/CharacterController.java
+++ src/main/java/com/disney/film/controllers/CharacterController.java
@@ -1,16 +1,10 @@
 package com.disney.film.controllers;
 
 import com.disney.film.models.CharacterModel;
-import com.disney.film.models.UserModel;
 import com.disney.film.models.dto.CharacterDTO;
 import com.disney.film.services.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/characters")
@@ -34,5 +28,19 @@ public class CharacterController {
         return this.characterService.getCharacterByIdFilm(id);
     }
 
+    @PostMapping()
+    public CharacterModel saveCharacter(@RequestBody CharacterModel characterModel) {
+        return this.characterService.saveCharacter(characterModel);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteById(@PathVariable("id") Long id) {
+        boolean ok = this.characterService.deleteCharacter(id);
+        if (ok) {
+            return "Deleted character by id:" + id;
+        } else {
+            return "Could not delete character with id:" + id;
+        }
+    }
 
 }
